33fb9281e1ff730021ea56f98e49d857742141fe
t1dmlgus <dmlgusgngl@gmail.com>
2021-07-02 17:19:54 +0900

    Initial commit

---
+++ src/main/java/com/s1dmlgus/codingTest02/CodingTest02Application.java
@@ -0,0 +1,13 @@
+package com.s1dmlgus.codingTest02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTest02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingTest02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/s1dmlgus/codingTest02/CodingTest02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1dmlgus.codingTest02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTest02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
